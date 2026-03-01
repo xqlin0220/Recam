940eab8fff66756e907a3fe80bb21e55b42ece34
Xuqing Lin <xlin0057@student.monash.edu>
2026-03-01 16:36:26 +1100

    feat(photographyCompany): implement API to retrieve agents under current company account

--- src/Remp.Service/Interfaces/IPhotographyCompanyService.cs
+++ src/Remp.Service/Interfaces/IPhotographyCompanyService.cs
@@ -1,6 +1,8 @@
+using Remp.Service.DTOs;
 namespace Remp.Service.Interfaces;
 
 public interface IPhotographyCompanyService
 {
     Task AddAgentAsync(string photographyCompanyUserId, string agentId);
+    Task<List<AgentSummaryDto>> GetAgentsAsync(string photographyCompanyUserId);
 }
